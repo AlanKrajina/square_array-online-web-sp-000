252ffc3074e9a7ddf29e4d9129a27fae098c7e60
Alan Krajina <alankrajina88@gmail.com>
2019-05-09 15:25:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,9 @@ def square_array(array)
   new_array=[]
   array.each do |numb|
   numb *= numb
-  return new_array.push(numb)
+  new_array.push(numb)
   end
+  return new_array
 end
 
 square_array([1,2,3])
